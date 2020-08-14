66dc1a96e476c245afea538e82d9b529c4954bcf
Fabio Niglio <fabioniglio@gmail.com>
2020-08-14 15:32:53 +0100

    teacher list

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 
+import "./styles.css";
+
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="That is awesome that you want to give classes"></PageHeader>
+      <PageHeader
+        description="The first step is to fill the register form!"
+        title="That is awesome that you want to give classes"
+      ></PageHeader>
+
+      <main>
+        <fieldset>
+          <legend>Your Information</legend>
+          <div className="input-block">
+            <label htmlFor="name">Full Name</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
